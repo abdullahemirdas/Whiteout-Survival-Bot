20740f593d06dc68157a8aa73e1910e3ff95bc3b
abdullahemirdas <83994930+abdullahemirdas@users.noreply.github.com>
2026-08-01 01:33:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+
+from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 
@@ -24,87 +27,77 @@ from scheduler import (
 
 
 
-async def main():
+async def health_check(request):
+
+    return web.Response(
+        text="Whiteout ATA Bot aktif"
+    )
 
 
-    # =========================
-    # DATABASE
-    # =========================
+
+async def start_web_server():
+
+    app = web.Application()
+
+    app.router.add_get(
+        "/",
+        health_check
+    )
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            10000
+        )
+    )
+
+    runner = web.AppRunner(app)
+
+    await runner.setup()
+
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        port
+    )
+
+    await site.start()
+
+
+    print(
+        f"Web server aktif: {port}"
+    )
+
+
+
+async def start_bot():
 
     await init_db()
 
-
-
-    # =========================
-    # BOT
-    # =========================
 
     bot = Bot(
         token=BOT_TOKEN
     )
 
 
-
-    # =========================
-    # SCHEDULER
-    # =========================
-
     set_bot(bot)
 
-
-
-    # =========================
-    # DISPATCHER
-    # =========================
 
     dp = Dispatcher()
 
 
+    dp.include_router(start.router)
+    dp.include_router(events.router)
+    dp.include_router(admin.router)
+    dp.include_router(repeat.router)
+    dp.include_router(broadcast.router)
+    dp.include_router(alliance.router)
+    dp.include_router(alliance_admin.router)
+    dp.include_router(contact.router)
 
-    # =========================
-    # ROUTERLAR
-    # =========================
-
-    dp.include_router(
-        start.router
-    )
-
-    dp.include_router(
-        events.router
-    )
-
-    dp.include_router(
-        admin.router
-    )
-
-    dp.include_router(
-        repeat.router
-    )
-
-    dp.include_router(
-        broadcast.router
-    )
-
-    dp.include_router(
-        alliance.router
-    )
-
-    dp.include_router(
-        alliance_admin.router
-    )
-
-    dp.include_router(
-        contact.router
-    )
-
-
-
-    # =========================
-    # SCHEDULER
-    # =========================
 
     start_scheduler()
-
 
 
     print(
@@ -112,10 +105,17 @@ async def main():
     )
 
 
-
     await dp.start_polling(
         bot
     )
+
+
+
+async def main():
+
+    await start_web_server()
+
+    await start_bot()
 
 
 
